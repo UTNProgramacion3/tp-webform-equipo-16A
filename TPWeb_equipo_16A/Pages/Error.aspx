8167c47bb6a70af7735e7485053c158a1e96487a
Franc00Baez <francobaez2309@gmail.com>
2024-10-06 14:53:04 -0300

    Implementando pagina de error

---
+++ TPWeb_equipo_16A/Pages/Error.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Main.Master" AutoEventWireup="true" CodeBehind="Error.aspx.cs" Inherits="TPWeb_equipo_16A.Pages.Error" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ TPWeb_equipo_16A/Pages/Error.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPWeb_equipo_16A.Pages
+{
+    public partial class Error : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verificamos si se ha pasado información de error
+            if (Session["ErrorMessage"] != null)
+            {
+                // Mostramos el mensaje de error en la página
+                litErrorMessage.Text = $"<p><strong>Error: </strong>{Session["ErrorMessage"]}</p>";
+            }
+
+            if (Session["StackTrace"] != null)
+            {
+                // Mostramos el StackTrace
+                litStackTrace.Text = $"<p><strong>Details:</strong></p><div>{Session["StackTrace"]}</div>";
+            }
+
+            // Limpiamos las variables de sesión para no seguir mostrando el error en futuras páginas
+            Session.Remove("ErrorMessage");
+            Session.Remove("StackTrace");
+
+        }
+    }
+}
