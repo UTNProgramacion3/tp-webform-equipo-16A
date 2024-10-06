895f9a167ea84e851597b7c9ac4a55cd15b9ee78
Franc00Baez <francobaez2309@gmail.com>
2024-10-06 15:06:24 -0300

    Refactor para Rewards, logError ex

--- TPWeb_equipo_16A/Pages/Rewards.aspx.cs
+++ TPWeb_equipo_16A/Pages/Rewards.aspx.cs
@@ -1,42 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Business.Managers;
 using Business.Dtos;
-using Utils;
+using System;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
 
 namespace TPWeb_equipo_16A.Pages
 {
-	public partial class Rewards : System.Web.UI.Page
-	{
+    public partial class Rewards : System.Web.UI.Page
+    {
         protected List<ArticuloDTO> Articulos;
         protected List<Imagen> Imagenes;
         protected ImagenManager imgManager;
         protected ArticuloManager artManager;
 
         protected void Page_Load(object sender, EventArgs e)
-		{
+        {
             if (!IsPostBack)
             {
-                if(Session["VoucherValidado"] != null)
+                if (Session["VoucherValidado"] != null)
                 {
-                    InitializeManagers();
-                    Articulos = artManager.ObtenerTodos();
-
-                    foreach (var articuloDTO in Articulos)
+                    try
                     {
-                        var imagenes = imgManager.ObtenerImagenesPorArticulo(articuloDTO.Articulo.Id);
+                        InitializeManagers();
+                        Articulos = artManager.ObtenerTodos();
 
-                        if(imagenes != null)
-                        articuloDTO.Imagenes = Utils.ObjectConverter.ConvertToStringList(imagenes, i => i.ImagenUrl);
+                        foreach (var articuloDTO in Articulos)
+                        {
+                            try
+                            {
+                                var imagenes = imgManager.ObtenerImagenesPorArticulo(articuloDTO.Articulo.Id);
+
+                                if (imagenes != null)
+                                    articuloDTO.Imagenes = Utils.ObjectConverter.ConvertToStringList(imagenes, i => i.ImagenUrl);
+                            }
+                            catch (Exception ex)
+                            {
+                                RedirectToErrorPage(ex);
+                            }
+                        }
+
+                        repRepeater.DataSource = Articulos;
+                        repRepeater.DataBind();
                     }
-
-                    repRepeater.DataSource = Articulos;
-                    repRepeater.DataBind();
+                    catch (Exception ex)
+                    {
+                        RedirectToErrorPage(ex);
+                    }
                 }
                 else
                 {
@@ -53,28 +63,50 @@ namespace TPWeb_equipo_16A.Pages
 
         protected void btnSeleccionar_Click(object sender, EventArgs e)
         {
-            var button = (Button)sender;
-
-            var articuloId = button.CommandArgument;
-
-            Session.Add("ArticuloSeleccionado", articuloId);
-
-            Response.Redirect("~/Pages/Participar.aspx");
-        }
-
-        protected void repRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
-        {
-            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            try
             {
-                var articuloDto = (ArticuloDTO)e.Item.DataItem;
-                var repImagenes = (Repeater)e.Item.FindControl("repImagenes");
+                var button = (Button)sender;
+                var articuloId = button.CommandArgument;
 
-                if (articuloDto.Imagenes != null && articuloDto.Imagenes.Count > 0)
-                {
-                    repImagenes.DataSource = articuloDto.Imagenes;
-                    repImagenes.DataBind();
-                }
+                Session.Add("ArticuloSeleccionado", articuloId);
+
+                Response.Redirect("~/Pages/priceResult.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                RedirectToErrorPage(ex);
             }
         }
+
+        protected void repRepeater_ItemDataBound(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
+        {
+            try
+            {
+                if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+                {
+                    var articuloDto = (ArticuloDTO)e.Item.DataItem;
+                    var repImagenes = (Repeater)e.Item.FindControl("repImagenes");
+
+                    if (articuloDto.Imagenes != null && articuloDto.Imagenes.Count > 0)
+                    {
+                        repImagenes.DataSource = articuloDto.Imagenes;
+                        repImagenes.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                RedirectToErrorPage(ex);
+            }
+        }
+
+        private void RedirectToErrorPage(Exception ex)
+        {
+            Session["ErrorMessage"] = ex.Message;
+            Session["StackTrace"] = ex.StackTrace;
+            Response.Redirect("~/Pages/Error.aspx");
+        }
+
+
     }
 }
