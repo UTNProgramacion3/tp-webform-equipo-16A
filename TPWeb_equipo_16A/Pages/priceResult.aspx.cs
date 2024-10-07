d99772d8a6cef3f7ad03f1d1848fbb19a68c7e50
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2024-10-06 22:38:21 -0300

    Se elimina session

--- TPWeb_equipo_16A/Pages/priceResult.aspx.cs
+++ TPWeb_equipo_16A/Pages/priceResult.aspx.cs
@@ -30,6 +30,7 @@ namespace TPWeb_equipo_16A.Pages
                 successCard.Visible = false;
                 failureCard.Visible = true;
             }
+            Session.Abandon();
             ClientScript.RegisterStartupScript(this.GetType(), "redirectScript", "setTimeout(function(){ window.location.href = 'checkVoucher.aspx'; }, 8000);", true);
 
         }
